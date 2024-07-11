85a0bb8d1d39f3cb72d54f858abd243b4e9fb42d
rafagomez92 <gomez92rafael@gmail.com>
2024-07-10 23:02:08 -0600

    feat: add meal item for show the information of the meal includes image and description

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  final String title;
+  final List<Meal> meals;
+  const MealsScreen({super.key, required this.title, required this.meals});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "Uh oh... nothing here!",
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onSurface),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            "Try selecting  a different category!",
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onSurface),
+          )
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: meals.length,
+          itemBuilder: (ctx, index) => MealItem(meal: meals[index]));
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
   }
 }
